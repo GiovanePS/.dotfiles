6b4d223bd71b77c1b1289aab13e2659a78926bd7
GiovanePS <giovanespace@gmail.com>
2024-06-15 12:48:49 -0300

    adding vim plug to init.lua for nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,29 @@ vim.opt.ttyfast = true
 -- Uncomment the following line to set the backup directory
 -- vim.opt.backupdir = '~/.cache/vim'
 
+vim.opt.rtp:append('usr/bin/fzf')
+
+--  VIM PLUGINS
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+
+-- FZF Dependencies:
+-- sudo nala install fzf bat ripgrep silversearcher-ag perl universal-ctags
+Plug('junegunn/fzf', { ['do'] = function()
+  vim.fn['fzf#install']()
+end })
+Plug('junegunn/fzf.vim')
+
+
+Plug('nvim-lua/plenary.nvim')
+
+
+vim.call('plug#end')
+
+
 -- Check if running in VSCode
 if vim.g.vscode then
   -- VSCode extension specific settings
