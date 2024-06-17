cb817f8bfbc04e1b1d0a051f2723667fd7c028c1
Giovane-Debian-Bridge <giovanespace@gmail.com>
2024-06-16 22:47:49 -0300

    feat: complete plugin for nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -25,7 +25,17 @@ Plug('neovim/nvim-lspconfig')
 Plug('ray-x/go.nvim')
 Plug('ray-x/guihua.lua'); -- recommended if need floating window support
 
+-- Complete
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+
 vim.call('plug#end')
 
+-- Requires
 require('plugins.colourscheme')
 require('go').setup()
+require('plugins.complete')
