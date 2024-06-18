4c651e8afd8809ccdc248a8cfb98b1125e5beb9b
GiovanePS <giovanespace@gmail.com>
2024-06-18 02:28:00 -0300

    feat: close buffer with \ + w

--- .config/nvim/lua/configs/map.lua
+++ .config/nvim/lua/configs/map.lua
@@ -1,11 +1,14 @@
 -- Bufferline
--- Map <Tab> to the save_and_next function
+-- Map Tab to the save_and_next function
 vim.api.nvim_set_keymap('n', '<Tab>', ':bnext <CR>', { noremap = true, silent = true })
 
--- Map <S-Tab> to the save_and_previous function
+-- Map Shift + Tab to the save_and_previous function
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprevious <CR>', { noremap = true, silent = true })
 
 
 -- Nvim Tree
+-- Map Control + Tab to Toggle NvimTree
 vim.api.nvim_set_keymap('n', '<C-Tab>', ':NvimTreeToggle <CR>', { noremap = true, silent = true })
 
+-- Map \ + w to Close Buffer (Close window)
+vim.api.nvim_set_keymap('n', '<leader>w', ':bd <CR>', { noremap = true, silent = true })
