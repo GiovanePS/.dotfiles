76474edfd1e6b24b2b79202498e07ad766657f24
Giovane-Debian-Bridge <giovanespace@gmail.com>
2024-06-19 12:46:41 -0300

    Position=float removed in map, removing send_text on ctrl+tab and updating README

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -1,6 +1,6 @@
 -- Nvim Tree
 -- Map to <Tab>
-vim.api.nvim_set_keymap("n", "<Tab>", ":Neotree position=float toggle <CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":Neotree toggle <CR>", { noremap = true, silent = true })
 
 -- Map \ + w to Close Buffer (Close window)
 vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
