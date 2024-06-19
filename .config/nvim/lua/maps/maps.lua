5bfa53f15f8d14607da7a9521f54be16729d80e2
GiovanePS <giovanespace@gmail.com>
2024-06-19 01:42:48 -0300

    removing bufferline keymaps

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -1,10 +1,3 @@
--- Bufferline
--- Map Tab to the save_and_next function
-vim.api.nvim_set_keymap("n", "<Tab>", ":bnext <CR>", { noremap = true, silent = true })
-
--- Map Shift + Tab to the save_and_previous function
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious <CR>", { noremap = true, silent = true })
-
 -- Nvim Tree
 -- Map Control + Tab to Toggle NvimTree
 vim.api.nvim_set_keymap("n", "<C-Tab>", ":Neotree toggle <CR>", { noremap = true, silent = true })
