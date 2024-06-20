49d445566efa647f10cef4513a5adebde5bcbcdd
Giovane-Debian-Bridge <giovanespace@gmail.com>
2024-06-19 21:06:14 -0300

    adding delete word forward

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -4,3 +4,6 @@ vim.api.nvim_set_keymap("n", "<Tab>", ":Neotree toggle <CR>", { noremap = true, 
 
 -- Map \ + w to Close Buffer (Close window)
 vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
+
+-- Map to Control + F delete the word forward in insert mode, like Control + delete
+vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>dwa", { noremap = true, silent = false })
---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/noice.nvim",
+  keys = {
+    { "<C-f>", false, mode = { "i", "n", "s" } },
+    { "<C-b>", false, mode = { "i", "n", "s" } },
+  },
+}
