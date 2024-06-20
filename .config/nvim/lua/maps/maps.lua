6481c65d73160456a5d029dc902f897e44e8424c
GiovanePS <giovanespace@gmail.com>
2024-06-20 14:28:08 -0300

    removing C-f and C-b to scroll up and scroll down

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -4,4 +4,10 @@
 vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
 
 -- Map to Control + F delete the word forward in insert mode, like Control + delete
-vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>dwa", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>ldwi", { noremap = true, silent = false })
+
+-- Removing keymaps to Control - F do Scroll up
+vim.api.nvim_set_keymap("n", "<C-f>", "<Nop>", { noremap = true, silent = false })
+
+-- Removing keymaps to Control- B do Scroll down
+vim.api.nvim_set_keymap("n", "<C-b>", "<Nop>", { noremap = true, silent = false })
