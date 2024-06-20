545936c64efa93d99e53f3cd82d828f9993e76b3
GiovanePS <giovanespace@gmail.com>
2024-06-20 03:41:16 -0300

    removing a keymap <Tab> to open NeoTree

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -1,6 +1,4 @@
 -- Nvim Tree
--- Map to <Tab>
-vim.api.nvim_set_keymap("n", "<Tab>", ":Neotree toggle <CR>", { noremap = true, silent = true })
 
 -- Map \ + w to Close Buffer (Close window)
 vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
