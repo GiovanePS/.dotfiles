6d2f65811b04aea1247ab906ff0c113cec4d390e
GiovanePS <giovanespace@gmail.com>
2024-06-24 14:58:11 -0300

    adding <C-d> in insert mode to delete character forward cursor, like <C-d> in terminal

--- .config/nvim/lua/maps/maps.lua
+++ .config/nvim/lua/maps/maps.lua
@@ -3,8 +3,11 @@
 -- Map \ + w to Close Buffer (Close window)
 vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
 
--- Map to Control + F delete the word forward in insert mode, like Control + delete
+-- Map to Control + F delete the word forward cursor in insert mode, like Control + delete
 vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>ldwi", { noremap = true, silent = false })
+
+-- Map to Control + D delete the character forward cursor in insert mode, like <Del>
+vim.api.nvim_set_keymap("i", "<C-d>", "<Esc>lxi", { noremap = true, silent = false })
 
 -- Remove keymaps to Control - F do Scroll up
 vim.api.nvim_set_keymap("n", "<C-f>", "<Nop>", { noremap = true, silent = false })
