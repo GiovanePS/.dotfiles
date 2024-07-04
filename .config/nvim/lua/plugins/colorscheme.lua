281a3e8e5856949a50fa7da01c246ac216f3f93a
GiovanePS <giovanespace@gmail.com>
2024-07-04 16:09:10 -0300

    feat: Monokai Soda transparent

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
 return {
   {
-    "scottmckendry/cyberdream.nvim",
+    "tanvirtin/monokai.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("cyberdream").setup({
+      require("monokai").setup({
+        palette = require("monokai").soda,
         transparent = true,
       })
-      vim.cmd([[colorscheme cyberdream]])
+      -- Ensure the colorscheme is applied
+      vim.cmd([[colorscheme monokai]])
+      -- Set the background of the Normal and other highlight groups to none after a short delay
+      vim.defer_fn(function()
+        vim.cmd([[
+          hi Normal guibg=none ctermbg=none
+          hi LineNr guibg=none ctermbg=none
+          hi SignColumn guibg=none ctermbg=none
+          hi CursorLineNr guibg=none ctermbg=none
+          hi Visual guibg=#2D2A2E
+        ]])
+      end, 100)
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "monokai",
     },
   },
 }
