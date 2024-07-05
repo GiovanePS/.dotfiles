3c1668f839199553c0680c86a79f2aab82739665
GiovanePS <giovanespace@gmail.com>
2024-07-05 00:18:45 -0300

    feat: gitsigns.lua config

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,24 @@
+-- ~/.config/nvim/lua/config/plugins.lua
+return {
+  -- other plugin configurations
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        -- other gitsigns configurations
+      })
+
+      -- Set custom highlights for GitSigns
+      vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#00ff00", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#ffff00", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#ff0000", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsAddNr", { fg = "#00ff00", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsChangeNr", { fg = "#ffff00", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { fg = "#ff0000", bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsAddLn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsChangeLn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { bg = "none" })
+    end,
+  },
+  -- other plugin configurations
+}
