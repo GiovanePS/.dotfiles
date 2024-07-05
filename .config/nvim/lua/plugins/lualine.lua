c9ef745beec2af7c477961eacce4fb39c3933484
GiovanePS <giovanespace@gmail.com>
2024-07-05 02:10:30 -0300

    Solving lualine bug in name files

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+-- ~/.config/nvim/lua/config/plugins.lua
+
+-- Load lualine plugin
+return {
+  -- Other plugin configurations
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "monokai_soda", -- Adjust theme as per your setup
+        },
+        sections = {
+          lualine_c = { { "filename", path = 1 } }, -- Display filename with full path
+        },
+        inactive_sections = {
+          lualine_c = {},
+        },
+      })
+
+      -- Set custom highlight for lualine filename
+      vim.cmd([[
+        highlight LualineFilename guifg=#f8f8f0 guibg=#272a30
+      ]])
+    end,
+  },
+  -- Other plugin configurations
+}
