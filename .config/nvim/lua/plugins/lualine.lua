f07d4ba13e0c01dede77e1b30c74aa9bd8c1218c
GiovanePS <giovanespace@gmail.com>
2024-07-07 18:38:09 -0300

    Returing to default lualine Lazyvim config and fix bug with modified_hl

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,125 @@
--- ~/.config/nvim/lua/config/plugins.lua
-
--- Load lualine plugin
 return {
-  -- Other plugin configurations
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto", -- Adjust theme as per your setup
-        },
-        sections = {
-          lualine_c = { { "filename", path = 1 } }, -- Display filename with full path
-        },
-        inactive_sections = {
-          lualine_c = {},
-        },
-      })
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
 
-      -- Set custom highlight for lualine filename
-      vim.cmd([[
-        highlight LualineFilename guifg=#f8f8f0 guibg=#272a30
-      ]])
-    end,
-  },
-  -- Other plugin configurations
+    local icons = LazyVim.config.icons
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    vim.api.nvim_set_hl(0, "LualineModified", { fg = "#FFFF00", bg = "none", bold = true })
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path({ modified_hl = "LualineModified" }) },
+        },
+        lualine_x = {
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = function() return LazyVim.ui.fg("Statement") end,
+          },
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return LazyVim.ui.fg("Constant") end,
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = function() return LazyVim.ui.fg("Debug") end,
+          },
+          -- stylua: ignore
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function() return LazyVim.ui.fg("Special") end,
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+
+    -- do not add trouble symbols if aerial is enabled
+    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+      local trouble = require("trouble")
+      local symbols = trouble.statusline
+        and trouble.statusline({
+          mode = "symbols",
+          groups = {},
+          title = false,
+          filter = { range = true },
+          format = "{kind_icon}{symbol.name:Normal}",
+          hl_group = "lualine_c_normal",
+        })
+      table.insert(opts.sections.lualine_c, {
+        symbols and symbols.get,
+        cond = symbols and symbols.has,
+      })
+    end
+
+    return opts
+  end,
 }
