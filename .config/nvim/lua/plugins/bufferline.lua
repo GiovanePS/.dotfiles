819fce75e0125724e854f1338051ea88609095dd
GiovanePS <giovanespace@gmail.com>
2024-09-03 12:48:18 -0300

    adding keybind to move bufferline positions

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,27 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("bufferline").setup({
-      options = {
-        always_show_bufferline = true,
-      },
-    })
-  end,
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          always_show_bufferline = true,
+        },
+      })
+
+      vim.api.nvim_set_keymap(
+        "n",
+        "<localleader>l",
+        ":BufferLineMoveNext<CR>",
+        { noremap = true, silent = true, desc = "Move bufferline next" }
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "<localleader>h",
+        ":BufferLineMovePrev<CR>",
+        { noremap = true, silent = true, desc = "Move bufferline previous" }
+      )
+    end,
+  },
 }
