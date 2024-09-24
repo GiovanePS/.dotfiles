0be210428bce353807785ab95abb24f9a3653149
GiovanePS <giovanespace@gmail.com>
2024-09-23 21:01:14 -0300

    Disabling spelling for all buffer types in Neovim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown" },
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
   command = "setlocal nospell",
 })
+
