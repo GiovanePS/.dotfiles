7d895c805a7a22a4fc09554eca93bf74dfea4c59
GiovanePS <giovanespace@gmail.com>
2024-11-21 03:15:14 -0300

    Removing old/unnecessary keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,22 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Nvim Tree
--- Map Tab to the save_and_next function
-vim.keymap.set("n", "<Tab>", ":bnext <CR>", { noremap = true, silent = true })
-
--- Map Shift + Tab to the save_and_previous function
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious <CR>", { noremap = true, silent = true })
-
--- Map Control + Tab to Toggle NvimTree
-vim.api.nvim_set_keymap("n", "<C-Tab>", ":Neotree <CR>", { noremap = true, silent = true })
-
--- Map \ + w to Close Buffer (Close window)
-vim.api.nvim_set_keymap("n", "<leader>w", ":bd <CR>", { noremap = true, silent = true })
-
--- Map \ + w to Close Buffer (Close window)
-vim.api.nvim_set_keymap("n", "<localleader>w", ":bdelete <CR>", { noremap = true, silent = true })
-
 -- Map to Control + F delete the word forward cursor in insert mode, like Control + delete
 vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>ldwi", { noremap = true, silent = false })
 
