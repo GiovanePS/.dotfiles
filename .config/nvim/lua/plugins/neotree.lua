3cf4c5f86434caad3368bbe99dbc32eff4b3140d
GiovanePS <giovanespace@gmail.com>
2024-11-20 23:34:52 -0300

    Changing config to opts in neotree.nvim

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,18 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  config = function()
-    require("neo-tree").setup({
-      window = {
-        position = "float",
-      },
-    })
-    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", fg = "#f6f6ec" })
-    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", fg = "#f6f6ec" })
-
-    -- Set custom highlights for NeoTree titles
-    vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { bg = "#211F22", fg = "#f6f6ec", bold = true })
-    -- vim.api.nvim_set_hl(0, "NeoTreeRootName", { bg = "#211F22", fg = "#78DCE8", bold = true })
-    -- vim.api.nvim_set_hl(0, "NeoTreeFileName", { bg = "none", fg = "#f6f6ec" })
-    -- vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { bg = "none", fg = "#97e023" })
+  opts = function(_, opts)
+    opts.window.position = "float"
   end,
 }
