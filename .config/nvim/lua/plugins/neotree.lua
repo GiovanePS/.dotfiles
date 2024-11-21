dca1f5960e565676460451db2a5be3293ae06c80
GiovanePS <giovanespace@gmail.com>
2024-11-21 02:52:36 -0300

    Color for some texts in Neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,5 +2,9 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
     opts.window.position = "float"
+
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", fg = "#f6f6ec" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", fg = "#f6f6ec" })
+    vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { bg = "#211F22", fg = "#f6f6ec", bold = true })
   end,
 }
