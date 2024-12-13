d217f5f982c995b598bf6df2f2c8f56e10d7df4e
GiovanePS <giovanespace@gmail.com>
2024-12-13 00:13:41 -0300

    Fix weird behaviour on set up colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,20 +6,20 @@ return {
     config = function()
       require("monokai").setup({
         palette = require("monokai").soda,
-        transparent = true,
       })
 
       vim.cmd([[colorscheme monokai]])
       vim.defer_fn(function()
         vim.cmd([[
-          hi Normal guibg=none ctermbg=none
-          hi LineNr guibg=none guifg=#999999 ctermbg=none
-          hi SignColumn guibg=none ctermbg=none
-          hi CursorLineNr guibg=none ctermbg=none
-          hi Visual guibg=#5F5A60
-          hi MatchParen cterm=bold ctermbg=none ctermfg=none guibg=#97e023 guifg=#333842
-        ]])
-      end, 100)
+        hi Normal guibg=none ctermbg=none
+        hi LineNr guibg=none guifg=#999999 ctermbg=none
+        hi SignColumn guibg=none ctermbg=none
+        hi CursorLineNr guibg=none ctermbg=none
+        hi Visual guibg=#5F5A60
+        hi MatchParen cterm=bold ctermbg=none ctermfg=none guibg=#97e023 guifg=#333842
+		hi MsgArea guibg=#222426
+      ]])
+      end, 200)
     end,
   },
 }
