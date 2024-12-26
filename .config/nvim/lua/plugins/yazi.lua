71da0d44649ea7aac76e6abd7ede95b690a02439
GiovanePS <giovanespace@gmail.com>
2024-12-26 01:50:17 -0300

    Add yazi.nvim and remove disable neotree

---
+++ .config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,30 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>e",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>E",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open yazi in working directory" ,
+    },
+    {
+      '<leader>t',
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
+}
