5325da9190889d86483b2e04d5cd74ca63df4025
GiovanePS <giovanespace@gmail.com>
2025-01-26 19:35:06 -0300

    Update keymaps to correctly run as expected

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,10 +3,26 @@
 -- Add any additional keymaps here
 
 -- Map to Control + F delete the word forward cursor in insert mode, like Control + delete
-vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>ldwi", { noremap = true, silent = false })
+vim.keymap.set("i", "<C-f>", function()
+    local _, column = unpack(vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()))
+    if column == 0 then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>dwi", true, false, true), "n", true)
+        return
+    end
+
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>ldwi", true, false, true), "n", true)
+end, { noremap = true, silent = false })
 
 -- Map to Control + D delete the character forward cursor in insert mode, like <Del>
-vim.api.nvim_set_keymap("i", "<C-d>", "<Esc>lxa", { noremap = true, silent = false })
+vim.keymap.set("i", "<C-d>", function()
+    local _, column = unpack(vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()))
+    if column == 0 then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>xi", true, false, true), "n", true)
+        return
+    end
+
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>lxi", true, false, true), "n", true)
+end, { noremap = true, silent = false })
 
 -- Map to Leader+y copy to clipboard
 vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
